16e4715a4486708f3a9fe22044414c57f68387d1
FilipeVBF <filipevbf@gmail.com>
2024-06-10 21:49:58 -0300

    ajuste css

--- To-do List/src/components/Task.tsx
+++ To-do List/src/components/Task.tsx
@@ -1,11 +1,30 @@
-import styles from 'Task.module.css';
+import { ClipboardText } from '@phosphor-icons/react';
+import styles from './Task.module.css';
 
 export function Task() {
   return (
-    <section>
-      <header>
-        
+    <section className={styles.section}>
+      <header className={styles.info}>
+        <div className={styles.created}>
+          <span>Tarefas criadas</span>
+          <div>
+            <p>0</p>
+          </div>
+        </div>
+        <div className={styles.done}>
+          <span>Concluídas</span>
+          <div>
+            <p>0</p>
+          </div>
+        </div>
       </header>
+      <div className={styles.container}>
+        <ClipboardText size={56} />
+        <span className={styles.empty}>
+          <p>Você ainda não tem tarefas cadastradas</p>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </span>
+      </div>
 
     </section>
   )
